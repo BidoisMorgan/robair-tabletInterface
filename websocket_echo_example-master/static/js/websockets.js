ec1043607063f39caadbcfb429bc91acffef82b9
thomas <thomas@ganicus.(none)>
2013-03-18 11:03:02 +0100

    transmision commande joeystick avec websockets.

--- websocket_echo_example-master/static/js/websockets.js
+++ websocket_echo_example-master/static/js/websockets.js
@@ -1,29 +1,41 @@
- $(document).ready(function(){
+$(document).ready(function() {
 
-                if ("WebSocket" in window) {
-                var loc = window.location, new_uri;
-                if (loc.protocol === "https:") {
-                    new_uri = "wss:";
-                } else {
-                    new_uri = "ws:";
-                }
-                new_uri += "//" + loc.host;
-                new_uri += loc.pathname + "/api";
-                ws = new WebSocket(new_uri);
-                ws.onmessage = function (msg) {
-                    $("#log").append("<p>"+msg.data+"</p>")
-                };
-            } else {
-                alert("WebSocket not supported");
-            }
-        });
-        
-        function advance(){
-	    ws.send($('#top').val())
-	    return true;
-        }
-        
-        function moonwalk(){
-	    ws.send($('#back').val())
-	    return true;
-        }
+	if ("WebSocket" in window) {
+		var loc = window.location, new_uri;
+		if (loc.protocol === "https:") {
+			new_uri = "wss:";
+		} else {
+			new_uri = "ws:";
+		}
+		new_uri += "//" + loc.host;
+		new_uri += loc.pathname + "/api";
+		ws = new WebSocket(new_uri);
+		ws.onmessage = function(msg) {
+			$("#log").append("<p>" + msg.data + "</p>")
+		};
+	} else {
+		alert("WebSocket not supported");
+	}
+});
+
+//Sending methods for directions.
+function goTop() {
+	ws.send($('#top').val())
+	return true;
+}
+
+function goBack() {
+	ws.send($('#back').val())
+	return true;
+}
+
+function goRight() {
+	alert("totoR");
+	ws.send("right");
+//	return true;
+}
+
+function goLeft() {
+	ws.send("left");
+//	return true;
+}
