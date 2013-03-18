a593850ee70f09dbc83a62330e1bc6dab1521bed
thomas <thomas@ganicus.(none)>
2013-03-18 09:50:01 +0100

    Revert "tr"
    This reverts commit 1d0963165d8394ba147f172d9eaaf6abde0683e6.

--- websocket_echo_example-master/static/js/websockets.js
+++ websocket_echo_example-master/static/js/websockets.js
@@ -1,37 +1,29 @@
-$(document).ready(function() {
+ $(document).ready(function(){
 
-	if ("WebSocket" in window) {
-		var loc = window.location, new_uri;
-		if (loc.protocol === "https:") {
-			new_uri = "wss:";
-		} else {
-			new_uri = "ws:";
-		}
-		new_uri += "//" + loc.host;
-		new_uri += loc.pathname + "/api";
-		ws = new WebSocket(new_uri);
-		ws.onmessage = function(msg) {
-			$("#log").append("<p>" + msg.data + "</p>")
-		};
-	} else {
-		alert("WebSocket not supported");
-	}
-});
-
-function goTop() {
-	ws.send($('#top').val())
-	return true;
-}
-
-function goRight() {
-	ws.send($('#right').val())
-	return true;
-}
-function goBack() {
-	ws.send($('#back').val())
-	return true;
-}
-function goLeft() {
-	ws.send($('#left').val())
-	return true;
-}
+                if ("WebSocket" in window) {
+                var loc = window.location, new_uri;
+                if (loc.protocol === "https:") {
+                    new_uri = "wss:";
+                } else {
+                    new_uri = "ws:";
+                }
+                new_uri += "//" + loc.host;
+                new_uri += loc.pathname + "/api";
+                ws = new WebSocket(new_uri);
+                ws.onmessage = function (msg) {
+                    $("#log").append("<p>"+msg.data+"</p>")
+                };
+            } else {
+                alert("WebSocket not supported");
+            }
+        });
+        
+        function advance(){
+	    ws.send($('#top').val())
+	    return true;
+        }
+        
+        function moonwalk(){
+	    ws.send($('#back').val())
+	    return true;
+        }
