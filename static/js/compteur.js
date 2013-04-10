4e26fe949d130d2a3ef68410e6d640fdf99fc464
thomas <thomas@ganicus.(none)>
2013-04-10 12:44:07 +0200

    compteur en sec et pas d'affichage debug

--- static/js/compteur.js
+++ static/js/compteur.js
@@ -1,11 +1,13 @@
 $(function() {
-	// possibilit� de donner une date pour la limite et ne rien changer en dessous
-	// si la date est d�pass�e comme ici vous pouvez r�gler en nb de jours en dessous
+
+if (document.body){
+  var larg = (document.body.clientWidth);
+  var haut = (document.body.clientHeight);
+  if (larg + haut >1000){
 	var note = $('#note'), ts = new Date(2012, 0, 1), newYear = true;
 
 	if ((new Date()) > ts) {
-		// dans ce cas 10*24*60*60*1000 repr�sente 10j 24h 60' 60"
-		// pour 2h on �crirait 1*2*60*60*1000
+		// in this case 10*24*60*60*1000 represent 10j 24h 60' 60"
 		// IMPORTANT : ne pas supprimer le *1000
 		ts = (new Date()).getTime() + 1 * 2 * 60 * 60 * 1000;
 		newYear = false;
@@ -16,13 +18,13 @@ $(function() {
 		callback : function(days, hours, minutes, seconds) {
 
 			var message = "";
-			message += hours + " heure" + (hours == 1 ? '' : 's' ) + ", ";
-			message += minutes + " minute" + (minutes == 1 ? '' : 's' ) + " and ";
-			message += seconds + " seconde" + (seconds == 1 ? '' : 's' ) + " <br />";
+			message += hours + " hour" + (hours == 1 ? '' : 's' ) + ", ";
+			message += minutes + " min" + (minutes == 1 ? '' : 's' ) + " and ";
+			//message += seconds + " sec" + (seconds == 1 ? '' : 's' ) + " <br />";
 
 			if (newYear) {
-				message += "jusqu'� la fin";
-				// ou indiquez la date
+				message += "jusqu a la fin";
+				// date
 			} else {
 				message += ".";
 			}
@@ -30,5 +32,6 @@ $(function() {
 			note.html(message);
 		}
 	});
-
+     }
+  }
 }); 
